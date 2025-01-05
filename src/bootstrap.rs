1f37495a63f35a978989eaee68cce5e2b72f37c3
Alex Szabo <kreatemore@gmail.com>
2025-01-05 21:03:32 +0100

    Extract session layer construction

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -1,14 +1,35 @@
-use dotenv;
+use std::env;
+use std::time::Duration;
+
+use axum_login::axum_sessions::{PersistencePolicy, SameSite, SessionLayer};
+use axum_login::axum_sessions::async_session::SessionStore;
 
 pub fn read_secret_from_env() -> [u8; 64] {
-    dotenv::dotenv().ok();
+    let secret = env::var("APP_SECRET").expect("App Secret is either undefined or not exactly 64 char long!");
+    let mut secret_bytes = [0; 64];
+    secret_bytes.copy_from_slice(secret.as_bytes());
 
-    let mut secret: [u8; 64] = [0; 64];
-    secret.copy_from_slice(
-        std::env::var("APP_SECRET")
-            .expect("App Secret is either undefined or not exactly 64 char long!")
-            .as_bytes(),
-    );
+    secret_bytes
+}
 
-    secret
+pub fn build_session_layer(
+    session_storage: impl SessionStore,
+    secret: &[u8; 64],
+) -> SessionLayer<impl SessionStore> {
+    let session_duration_minutes = env::var("SESSION_LIFETIME_MINUTES")
+        .unwrap_or("10".to_string())
+        .parse::<u64>()
+        .expect("Invalid session lifetime; can't convert to numeric value");
+    let is_secure_cookie = env::var("SECURE_COOKIE")
+        .unwrap_or("true".to_string())
+        .to_ascii_lowercase()
+        .eq("true");
+    let session_layer = SessionLayer::new(session_storage, secret)
+        .with_persistence_policy(PersistencePolicy::ExistingOnly)
+        .with_session_ttl(Some(Duration::from_secs(session_duration_minutes * 60)))
+        .with_same_site_policy(SameSite::Strict)
+        .with_http_only(true)
+        .with_secure(is_secure_cookie);
+
+    session_layer
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,11 @@ extern crate rust_i18n;
 
 use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::time::Duration;
-
-use axum::{middleware, routing::get, Extension, Router};
-use axum_login::axum_sessions::{PersistencePolicy, SameSite};
+use axum::{Extension, middleware, Router, routing::get};
 use axum_login::{
-    axum_sessions::SessionLayer, AuthLayer, PostgresStore, RequireAuthorizationLayer,
+    AuthLayer, PostgresStore, RequireAuthorizationLayer,
 };
-use bootstrap::read_secret_from_env;
 use dotenv::dotenv;
-use session::CookieStore;
 use sqlx::PgPool;
 
 use database::get_pool;
@@ -22,6 +17,7 @@ pub(crate) use crate::authn::models::User;
 use crate::authn::views;
 
 use crate::http::handler_404;
+use crate::session::CookieStore;
 
 mod authn;
 mod bootstrap;
@@ -45,26 +41,8 @@ pub enum Tracing {
 }
 
 pub async fn app(pool: PgPool, _with_tracing: Tracing) -> Router {
-    let secret = read_secret_from_env();
-
-    let session_duration_minutes = env::var("SESSION_LIFETIME_MINUTES")
-        .unwrap_or("10".to_string())
-        .parse::<u64>()
-        .expect("Invalid session lifetime; can't convert to numeric value");
-
-    let session_store = CookieStore::new();
-    let session_layer = SessionLayer::new(session_store, &secret)
-        .with_persistence_policy(PersistencePolicy::ExistingOnly)
-        .with_session_ttl(Some(Duration::from_secs(session_duration_minutes * 60)))
-        .with_same_site_policy(SameSite::Strict)
-        .with_http_only(true)
-        .with_secure(
-            env::var("SECURE_COOKIE")
-                .unwrap_or("true".to_string())
-                .to_ascii_lowercase()
-                .eq("true"),
-        );
-
+    let secret = bootstrap::read_secret_from_env();
+    let session_layer = bootstrap::build_session_layer(CookieStore::new(), &secret);
     let user_store = PostgresStore::<User>::new(pool.clone());
     let auth_layer = AuthLayer::new(user_store, &secret);
 
