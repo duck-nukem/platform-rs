36fb2152e37b56f4a28453b83fd02600950f581c
Alex Szabo <kreatemore@gmail.com>
2025-01-05 20:36:43 +0100

    Remove tracing
    due to breaking changes in the deprecated otlp-jaeger package

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,11 @@ use axum_login::{
 use dotenv::dotenv;
 use session::CookieStore;
 use sqlx::PgPool;
-use tower_http::trace::TraceLayer;
-use tower_request_id::RequestIdLayer;
-use tracing::Level;
-use tracing_subscriber::filter::LevelFilter;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 
 use database::get_pool;
 use http::{route_auth_guard, set_security_headers};
 
-use crate::authn::models::User;
+pub(crate) use crate::authn::models::User;
 use crate::authn::views;
 
 use crate::http::handler_404;
@@ -32,7 +26,7 @@ mod authn;
 mod database;
 mod deserialization;
 mod http;
-pub mod routing;
+mod routing;
 mod session;
 mod templates;
 
@@ -48,7 +42,7 @@ pub enum Tracing {
     Disabled,
 }
 
-pub async fn app(pool: PgPool, with_tracing: Tracing) -> Router {
+pub async fn app(pool: PgPool, _with_tracing: Tracing) -> Router {
     let mut secret: [u8; 64] = [0; 64];
     secret.copy_from_slice(
         env::var("APP_SECRET")
@@ -88,30 +82,10 @@ pub async fn app(pool: PgPool, with_tracing: Tracing) -> Router {
         .layer(auth_layer)
         .layer(session_layer)
         .layer(Extension(pool.clone()))
-        .layer(RequestIdLayer)
+        .layer(tower_request_id::RequestIdLayer)
         .layer(tower_http::compression::CompressionLayer::new())
         .layer(tower::ServiceBuilder::new().concurrency_limit(32))
         .fallback(handler_404);
-
-    match with_tracing {
-        Tracing::Enabled => {
-            let tracer = opentelemetry_jaeger::new_agent_pipeline()
-                .with_service_name("platform-rs")
-                .install_simple()
-                .expect("Telemetry Agent setup failed");
-            let trace_layer = TraceLayer::new_for_http()
-                .on_request(tower_http::trace::DefaultOnRequest::new().level(Level::INFO))
-                .make_span_with(tower_http::trace::DefaultMakeSpan::new().level(Level::INFO))
-                .on_response(tower_http::trace::DefaultOnResponse::new().level(Level::INFO));
-            tracing_subscriber::registry()
-                .with(LevelFilter::INFO)
-                .with(tracing_opentelemetry::layer().with_tracer(tracer))
-                .try_init()
-                .expect("Failed to register tracer with registry");
-            router = router.layer(trace_layer);
-        }
-        Tracing::Disabled => (),
-    }
 
     router = router.fallback(handler_404);
 
@@ -148,6 +122,4 @@ async fn main() {
         )
         .await
         .unwrap();
-
-    opentelemetry::global::shutdown_tracer_provider();
 }
